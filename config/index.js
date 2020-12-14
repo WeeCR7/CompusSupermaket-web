5197eacc2ece92087d6634a2b8c1cf255fe63b8a
zhanjiawei <1006099569@qq.com>
2020-12-14 16:54:09 +0800

    【校园超市系统】修改启动时的端口号

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/supermarket':{
-        target:'http://localhost:9999',
+        target:'http://47.96.88.202:9999/',
         changeOrigin: true,
         pathRewrite: {
           '^/':''
@@ -22,7 +22,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 10001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
