42478951750b4a83003a40a8ad82682de3f371ca
zhanjiawei <1006099569@qq.com>
2021-01-05 11:09:58 +0800

    【校园超市系统】添加面包屑，修改左侧导航栏的样式

--- src/layout/components/asideMenu.vue
+++ src/layout/components/asideMenu.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
-    <span class="el-icon-s-unfold" @click="handleOpenOrCloseMenu"></span>
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :collapse="isCollapse">
-      <el-submenu index="1">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <span>导航一</span>
         </template>
         <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <template slot="title">分组一</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
         </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="2">
+      <el-menu-item index="3">
         <i class="el-icon-menu"></i>
         <span slot="title">导航二</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
+      <el-menu-item index="4" disabled>
         <i class="el-icon-document"></i>
         <span slot="title">导航三</span>
       </el-menu-item>
-      <el-menu-item index="4">
+      <el-menu-item index="5">
         <i class="el-icon-setting"></i>
         <span slot="title">导航四</span>
       </el-menu-item>
@@ -55,7 +59,7 @@ export default {
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+  width: 180px;
   height: 100%;
 }
 
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,28 +1,95 @@
 <template>
-  <el-container style="height: 100%;min-height:100vh;" direction="vertical">
-    <el-header style="background-color: #B3C0D1;color: #333;height: 80px">
-      <img src="@/assets/myLogo.png" style="width: 400px;height: 80px;"/>
-
-    </el-header>
+  <el-container class="content" direction="vertical">
     <el-container>
       <aside-menu></aside-menu>
-      <el-main style="background-color: #E9EEF3;color: #333;">Main</el-main>
+      <el-main class="main">
+        <bread-crumb></bread-crumb>
+        <el-tabs class="tabs" v-model="editableTabsValue" type="card" closable="true" @edit="handleTabsEdit">
+          <el-tab-pane
+            :key="item.name"
+            v-for="(item, index) in editableTabs"
+            :label="item.title"
+            :name="item.name"
+          >
+            {{item.content}}
+          </el-tab-pane>
+        </el-tabs>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 import AsideMenu from './components/asideMenu'
+import BreadCrumb from "./components/breadCrumb";
 
 export default {
   name: "layout",
   components: {
+    BreadCrumb,
     AsideMenu
+  },
+  data() {
+    return {
+      editableTabsValue: '2',
+      editableTabs: [{
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content'
+      }, {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content'
+      }],
+      tabIndex: 2
+    }
+  },
+  methods: {
+    handleTabsEdit(targetName, action) {
+      if (action === 'add') {
+        let newTabName = ++this.tabIndex + '';
+        this.editableTabs.push({
+          title: 'New Tab',
+          name: newTabName,
+          content: 'New Tab content'
+        });
+        this.editableTabsValue = newTabName;
+      }
+      if (action === 'remove') {
+        let tabs = this.editableTabs;
+        let activeName = this.editableTabsValue;
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1];
+              if (nextTab) {
+                activeName = nextTab.name;
+              }
+            }
+          });
+        }
+
+        this.editableTabsValue = activeName;
+        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+      }
+    }
   }
 }
 </script>
 
 <style>
-
+.content{
+  height: 100%;
+  min-height:100vh;
+}
+.main{
+  background-color: white;
+  color: #333;
+}
+.tabs{
+  margin-top: 30px;
+  margin-left: 0;
+  height: 100%;
+}
 
 </style>
